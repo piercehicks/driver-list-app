623f0765bcc2eeb33f5e82a72efca6aca27e992a
Pierce Hicks <phicks21@gmail.com>
2020-06-20 15:13:44 -0400

    Basic form setup

--- src/components/CreateDriver.js
+++ src/components/CreateDriver.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 class CreateDriver extends Component {
     render() {
         return (
-            <div>
-                Edit Drive Comp
+            <div class="form-wrapper">
+                <Form>
+                    <Form.Group controlId="Name">
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control type="text"/>
+                    </Form.Group>
+                    <Form.Group controlId="Email">
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email"/>
+                    </Form.Group>
+                    <Form.Group controlId="Name">
+                        <Form.Label>Driver ID</Form.Label>
+                        <Form.Control type="text"/>
+                    </Form.Group>
+                        <Button variant="info" size="lg" block="block" type="submit">
+                            Register Driver
+                        </Button>
+                </Form>
+                
             </div>
         )
     }
